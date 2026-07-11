e47256678a301c2d3d1c5e5340301c3e969d9537
Padmaja Amberkar <Padmajaamberkar@gmail.com>
2026-07-11 19:49:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import plotly.express as px
 from calculator import SolarCalculator
 from ai_chat import ask_ai
 from pdf_generator import generate_pdf
-from database import Database
+from database import save_customer
 
 
 # Page Config
